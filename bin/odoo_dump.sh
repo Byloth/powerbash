d9ee7a0f2b5048ba631036e6f18aaf6eb0162f9a
Matteo Bilotta <mbilotta@linkgroup.it>
2019-01-03 17:10:42 +0100

    [IMP] Added 'odoo_dump.sh' as script based on Odoo libs.

---
+++ bin/odoo_dump.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#
+
+source "$(dirname "${0}")/../lib/odoo.sh"
+
+# Is Postgres client available on this local machine?
+#
+readonly PG_DUMP="pg_dump"
+
+# Is Postgres client available inside a Docker container?
+#
+readonly POSTGRES="<postgres container>"
+readonly PG_DUMP="docker exec -i ${POSTGRES} pg_dump -U ${PGUSER}"
+
+readonly BACKUP_DIR="./backups"
+readonly TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+
+read -p "Database name: " DATABASE
+
+readonly FILEPATH="${BACKUP_DIR}/${DATABASE}"
+readonly FILENAME="${NAME}_${TIMESTAMP}.tar.gz"
+
+mkdir -p "${FILEPATH}"
+cd "${FILEPATH}"
+
+echo -e "Dumping database... \c"
+${PG_DUMP} -b -Fc -d "${DATABASE}" -O > "${DATABASE}.dump"
+echo -e "\033[0;32mOK!\033[0m"
+
+echo -e "Copying filestore... \c"
+docker cp "${NAME}:/var/lib/odoo/filestore/${DATABASE}" . &> /dev/null
+echo -e "\033[0;32mOK!\033[0m"
+
+cd ..
+
+echo -e "Compressing backup... \c"
+tar -czvf "${FILENAME}" "${DATABASE}" &> /dev/null
+echo -e "\033[0;32mOK!\033[0m"
+
+echo -e "Removing temporary directories... \c"
+rm -rf "${DATABASE}" &> /dev/null
+echo -e "\033[0;32mOK!\033[0m"
+
+echo -e "\nDone!"
+echo -e " └ $ $(dirname "${0}")/odoo_restore.sh \"${BACKUP_DIR}/${FILENAME}\"\n"
